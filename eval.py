f597200c65217922c5c52891823a2df6247943f6
124coffee <823266085@qq.com>
2022-06-06 03:35:02 -0400

    evaluation function

--- eval.py
+++ eval.py
@@ -4,17 +4,17 @@ import numpy as np
 # 绘制混淆矩阵
 def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
     """
-    - cm : 计算出的混淆矩阵的值
-    - classes : 混淆矩阵中每一行每一列对应的列
-    - normalize : True:显示百分比, False:显示个数
+    - cm :  calculate the value of the confusion matrix
+    - classes : class for every row/column
+    - normalize : True:show percentage, False:show counts
     """
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("显示百分比：")
+        print("show percentage：")
         np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
         print(cm)
     else:
-        print('显示具体数字：')
+        print('show specific number：')
         print(cm)
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -22,7 +22,6 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
-    # matplotlib版本问题，如果不加下面这行代码，则绘制的混淆矩阵上下只能显示一半，有的版本的matplotlib不需要下面的代码，分别试一下即可
     plt.ylim(len(classes) - 0.5, -0.5)
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
@@ -34,13 +33,20 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+#test data, can be edited according to the result in fact
 
-cnf_matrix = np.array([[8707, 64, 731, 164, 45],
-                      [1821, 5530, 79, 0, 28],
-                      [266, 167, 1982, 4, 2],
-                      [691, 0, 107, 1930, 26],
-                      [30, 0, 111, 17, 42]])
-attack_types = ['Normal', 'DoS', 'Probe', 'R2L', 'U2R']
+cnf_matrix = np.array([[187,2,1,5,5],
+                      [12,176,2,7,3],
+                      [0,0,198,2,0],
+                      [6,5,4,173,12],
+                      [1,3,1,9,186]])
+attack_types = ['Cloth_mask', 'No_face_mask', 'Surgical_mask', 'K95_mask', 'mask_worn_incorrectly']
 
 plot_confusion_matrix(cnf_matrix, classes=attack_types, normalize=True, title='Normalized confusion matrix')
 
+"""
+precision=
+accuracy=
+recall=
+f-measure=
+"""
