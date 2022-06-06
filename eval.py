c9a19acb3c3d419e5abe2f030aba381ba792d6e6
124coffee <823266085@qq.com>
2022-06-06 03:53:34 -0400

    evaluation function

--- eval.py
+++ eval.py
@@ -45,8 +45,41 @@ attack_types = ['Cloth_mask', 'No_face_mask', 'Surgical_mask', 'K95_mask', 'mask
 plot_confusion_matrix(cnf_matrix, classes=attack_types, normalize=True, title='Normalized confusion matrix')
 
 """
-precision=
-accuracy=
-recall=
-f-measure=
+precision=TP/(TP+FP)
+accuracy=(TP+TN)/(TP+TN+FP+FN)
+recall=TP/(TP+FN)
+f-measure=2*(Precision*Recall)/(Precision+Recall)
 """
+cloth_mask=cnf_matrix[0]
+No_face_mask=cnf_matrix[1]
+Surgical_mask=cnf_matrix[2]
+K95_mask=cnf_matrix[3]
+mask_worn_incorrectly=cnf_matrix[4]
+
+#for convinience , I use the specific number here. It will be changed later
+#For cloth_mask
+precision1=187/(187+12+0+6+1)
+accuracy1=(1000-187-176-198-173-186)/1000
+recall1=187/(187+2+1+5+5)
+fm1=2*(precision1*recall1)/(precision1+recall1)
+print("precision 1: ",precision1)
+#For No_face_mask
+precision2=176/()
+accuracy2=
+recall2=
+fm2=2*(precision2*recall2)/(precision2+recall2)
+#For Surgical_mask
+precision3=
+accuracy3=
+recall3=
+fm3=2*(precision3*recall3)/(precision3+recall3)
+#For K95_Mask
+precision4=
+accuracy4=
+recall4=
+fm4=2*(precision4*recall4)/(precision4+recall4)
+#For mask_worn_incorrectly
+precision5=
+accuracy5=
+recall5=
+fm5=2*(precision5*recall5)/(precision5+recall5)
