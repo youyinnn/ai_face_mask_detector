2f62d2e317e90f45ef2e9ea59938600d962efee2
Alexander Fulleringer <alexander.fulleringer@mail.mcgill.ca>
2022-06-06 23:31:31 -0400

    Made a base_CNN classifier. It sucks. Will continue working on it.

--- Models.py
+++ Models.py
@@ -7,12 +7,12 @@ import torchvision.transforms as T
 
 
 
-#A basic LinearNet to be used as a baseline and for debugging
+# A basic LinearNet to be used as a baseline and for debugging
 class LinearNet(torch.nn.Module):
-    def __init__(self, num_layers=2, layer_width=200):
+    def __init__(self, num_layers=3, layer_width=1000):
         super(LinearNet, self).__init__()
         self.layers = nn.ModuleList()
-        self.layers += [nn.Linear(3*256*256,layer_width), nn.ReLU(inplace=True)]
+        self.layers += [nn.Linear(3*256*256, layer_width), nn.ReLU(inplace=True)]
         for i in range(num_layers):
             self.layers += [nn.Linear(layer_width, layer_width), nn.ReLU(inplace=True)]
         #self.layers += [nn.LazyLinear(64), nn.ReLU(inplace=True)]
@@ -27,4 +27,34 @@ class LinearNet(torch.nn.Module):
             x = self.layers[i](x)
         x = self.task_head(x)
         #x = self.softmax(x)
+        return x
+
+class Base_CNN(torch.nn.Module):
+    def __init__(self, num_layers=2, layer_width=200):
+        super(Base_CNN, self).__init__()
+        self.layers = nn.ModuleList()
+        self.layers += [nn.Conv2d(3, 48, kernel_size=3), nn.ReLU(inplace=True)]
+        self.layers += [nn.Conv2d(48, 96, kernel_size=3, stride=2), nn.ReLU(inplace=True)]
+        self.layers += [nn.MaxPool2d(3, stride=1)]
+        self.layers += [nn.Conv2d(96, 96, kernel_size=3), nn.ReLU(inplace=True)]
+        self.layers += [nn.Conv2d(96, 192, kernel_size=3, stride=2), nn.ReLU(inplace=True)]
+        self.layers += [nn.MaxPool2d(3, stride=1)]
+        self.layers += [nn.Conv2d(192, 192, kernel_size=3), nn.ReLU(inplace=True)]
+        self.layers += [nn.Conv2d(192, 96, kernel_size=3, stride=2), nn.ReLU(inplace=True)]
+        #self.linear1 = nn.LazyLinear(256)
+        #self.linear2 = nn.Linear(512, 256)
+        # self.layers += [nn.LazyLinear(32), nn.ReLU(inplace=True)]
+        self.fc = nn.LazyLinear(5)
+
+    def forward(self, x):
+        N,C,H,W = x.shape
+        #x = x.reshape(x.shape[0], x.shape[1], -1)
+        # x = self.layers[0](x)
+        #x = x.reshape(x.shape[0], x.shape[1], 56, 56)
+        for i in range(0, len(self.layers)):
+            x = self.layers[i](x)
+        x = x.view(N, -1)
+        #x = self.linear1(x)
+        x = self.fc(x)
+        x = torch.softmax(x,dim=1)
         return x
