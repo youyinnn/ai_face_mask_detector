5dc13fbd2eb095971edd1a019ae77685c17223a8
124coffee <823266085@qq.com>
2022-06-08 02:11:27 -0400

    evaluation function

--- Models.py
+++ Models.py
@@ -226,3 +226,41 @@ class Less_Conv_CNN(torch.nn.Module):
         x = self.fc(x)
         # x = torch.softmax(x,dim=1)
         return x
+
+class Chen_CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv_layer = nn.Sequential(
+
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc_layer = nn.Sequential(nn.Dropout(p=0.1), nn.Linear(64 * 64 * 64, 1000), nn.ReLU(inplace=True),
+                                  nn.Linear(1000, 512), nn.ReLU(inplace=True), nn.Dropout(p=0.1), nn.Linear(512, 10)
+                                  )
+
+    def forward(self, x):
+
+        # conv layers
+        x = self.conv_layer(x)
+
+        # flatten
+        x = x.view(-1, (64 * 64 * 64))
+
+        # fc layer
+        x = self.fc_layer(x)
+
+        return x
